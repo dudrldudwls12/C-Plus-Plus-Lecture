c2a3eb903e52b5d03f796f00cee3e0d518c897c4
dudrldudwls12 <cacasi376@gmail.com>
2024-05-30 19:46:13 +0900

    (2024.5.30) 연관 컨테이너

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,196 +1,23 @@
 ﻿#include <iostream>
-#include <time.h>
-#include <conio.h>
-#include <windows.h>
-#include <vector>
-#include <list>
-#include <deque>
-
-#define UP 72
-#define LEFT 75
-#define RIGHT 77
-#define DOWN 80
+#include <set>
 
 using namespace std;
 
-void View(int health)
-{
-    for (int i = 0; i < health; i++)
-    {
-        cout << "♥";
-    }
-
-    cout << endl;
-    cout << endl;
-}
-
-void Judgment(vector<const char*> & note, const char* arrow, int & health)
-{
-    if (note.back() == arrow) 
-    {
-        note.pop_back(); 
-    }
-    else
-    {
-        health--;
-    }
-}
-
 int main()
 {
-#pragma region 선형 컨테이너
-    // 데이터를 선형으로 저장하며, 특별한 제약이나 규칙이 없는 일반적인
-    // 컨테이너 입니다.
+#pragma region 연관 컨테이너
+    // 컨테이너 내의 자료들이 규칙에 따라 정렬되어 있는 컨테이너입니다.
 
-#pragma region Vector
+    std::set<int> set;
 
-    // std::vector<int> vector;
-    
-    // vector.reserve(6);
-    
-    // cout << "vector의 capacity 값 : " << vector.capacity() << endl;
-    
-    // vector.push_back(10); // [10]
-    // vector.push_back(20); // [10][20]
-    // vector.push_back(30); // [10][20][30]
-    // vector.push_back(40); // [10][20][30][40]
-    
-    // vector.pop_back();      // [10][20][30]
-    
-    // vector.clear()''
-    
-    // for (int i = 0; i < vector.size(); i++)
-    // {
-    //     cout << "vector[" << i << "]의 값 : " << vector[i] << endl;
-    // }
+    set.insert(50);
+    set.insert(10);
+    set.insert(80);
+    set.insert(30);
+    set.insert(65);
+    set.insert(45);
 
-#pragma endregion
-
-#pragma region Rhythm Game
-   
-    // srand(time(NULL));
-    // 
-    // std::vector<const char*> note;
-    // char key = 0;
-    // int count = 4;
-    // int health = 5;
-    // 
-    // note.reserve(6);
-    // 
-    //     // 1. Note를 생성합니다.
-    //     for (int i = 0; i < count; i++)
-    //     {
-    //         int random = rand() % 4;
-    // 
-    //         switch (random)
-    //         {
-    //         case 0: note.push_back("↑");
-    //             break;
-    //         case 1: note.push_back("←");
-    //             break;
-    //         case 2: note.push_back("→");
-    //             break;
-    //         case 3: note.push_back("↓");
-    //             break;
-    //         }        
-    //     }
-    // 
-    // while (health > 0)
-    // {
-    //     View(health);
-    // 
-    //     for (const char* element : note)
-    //     {
-    //         cout << element << "  ";
-    //     }
-    // 
-    //     key = _getch();
-    // 
-    //     if (key == -32)
-    //     {
-    //         key = _getch();
-    //     }
-    // 
-    //     switch (key)
-    //     {
-    //     case UP      : Judgment(note, "↑", health);
-    //         break;
-    //     case LEFT   : Judgment(note, "←", health);
-    //         break;
-    //     case RIGHT : Judgment(note, "→", health);
-    //         break;
-    //     case DOWN : Judgment(note, "↓", health);
-    //         break;
-    //     }
-    // 
-    //     if (note.size() <= 0)
-    //     {
-    //         break;
-    //     }
-    // 
-    //     system("cls"); 
-    // }
-    // 
-    // system("cls");
-    // 
-    // if (health <= 0)
-    // {
-    //     cout << "Defeat" << endl;
-    // }
-    // else
-    // {
-    //     cout << "Victory" << endl;
-    // }
-
-#pragma endregion
-
-#pragma region List
-
-     // std::list<int> list;
-
-     // list.push_back(10); // [10]
-     // list.push_back(10); // [10] [20]
-     
-     // list.push_front(5);   // [5] [10] [20]
-     // list.push_front(1);   // [1] [5] [10] [20]
-      
-     // list.pop_back();      // [1] [5] [10]
-     // list.pop_front();      // [5] [10]
-      
-     // cout << "list.front()  :  " << list.front() << endl;
-     // cout << "list.back()  :  " << list.back() << endl;
-#pragma endregion
-
-#pragma region String
-     // std::string name = "Alistar";
-     
-     // name.append(" Champion");
-     
-     // if (name.compare("Alistar"))
-     // {
-     //     cout << "문자열이 같습니다." << endl;
-     // }
-     
-     // name.clear();
-     
-     // cout << "name 변수의 값 : " << name << endl;
-#pragma endregion
-
-#pragma region Deque
-
-        // std::deque<int> deque;
-        
-        // deque.push_front(10);
-        // deque.push_back(20);
-        // deque.push_back(30);
-        // deque.push_back(40);
-        
-        // for (int i = 0; i < deque.size(); i++)
-        // {
-        //     cout << "deque [" << i << "]의 값 : " << deque[i] << endl;
-        // }
-#pragma endregion
-
+    // set의 경우 KEY 값이 중복이 되면 값이 그대로 들어옵니다.
 #pragma endregion
 
     return 0;
